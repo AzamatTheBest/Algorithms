6a435fffb3dd02b5bd7044c66f1794d4d4776a21
Azamat Tskhovrebov <AzamatTheBest.cs.py@gmail.com>
2020-11-20 15:23:31 +0300

    Update quicksort.c

--- C/quicksort.c
+++ C/quicksort.c
@@ -10,13 +10,16 @@ int main(int argc, char **argv)
     quicksort(array, 0, size - 1);
 
     int i;
-    for (i = 0; i < size; i++) printf("%d ", array[i]);
+    for (i = 0; i < size; i++)
+    {
+	printf("%d ", array[i]);
+    }
     printf("\n");
     return 0;
 }
 
 
-void quicksort(int array[], int low, int high)
+void quicksort(int *array, int low, int high)
 {
     int pi;
     
@@ -29,27 +32,29 @@ void quicksort(int array[], int low, int high)
     }
 }
 
-int partition(int array[], int low, int high)
+int partition(int *array, int low, int high)
 {
     int pivot, temp, i, j;
 
-    pivot = array[low];
-    i = low + 1;
+    pivot = array[high];
+    i = low - 1;
 
-    for (j = i + 1; j <= high; j++)
+    for (j = low; j <= high - 1; j++)
     {
-	if (array[j] <= pivot)
+	if (array[j] < pivot)
 	{
+	    i++;
 	    temp = array[j];
 	    array[j] = array[i];
 	    array[i] = temp;
-	    i++;
+	    
 	}
     }
 
-    temp = array[low];
-    array[low] = array[i];
-    array[i] = temp;
+    temp = array[high];
+    array[high] = array[i + 1];
+    array[i + 1] = temp;
 
-    return i;
+    return i + 1;
 }
+
