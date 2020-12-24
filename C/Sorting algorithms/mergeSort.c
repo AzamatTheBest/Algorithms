fb1f499a1b338f8f0428b8b186b4e531d6b8c4d0
Azamat Tskhovrebov <AzamatTheBest.cs.py@gmail.com>
2020-12-24 20:54:20 +0300

    Update mergeSort.c

--- C/Sorting algorithms/mergeSort.c
+++ C/Sorting algorithms/mergeSort.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 {
     int array[5] = {5,3,4,2,1};
     int size = sizeof(array) / sizeof(int);
-		merge_sort(array, 0, size - 1);
+    merge_sort(array, 0, size - 1);
 
     int i;
     for (i = 0; i < size; i++)
